0a6546e170d36602d3cb4b17d36872ee99e4f7e6
Tatev2919 <tatev@karapetian.ru>
2021-01-18 16:35:41 +0400

    111

--- LFSR.v
+++ LFSR.v
@@ -1,23 +1,23 @@
 module LFSR_for_hc(
 clk,rst,data_out
 );
+
 input wire clk,rst;
-output reg [15:0] data_out; 
+output [15:0] data_out; 
 wire polyn_data,polyn_data_for0;
 reg [15:0] random;
 
 assign polyn_data = (random[15]^random[14]^random[12]^random[3]);
-assign polyn_data_for0 = ~random[15]||~random[14]||~random[12]||~random[3];
+assign polyn_data_for0 = ~random[15]&~random[14]&~random[12]&~random[3];
+assign data_out = {random[14:0],polyn_data||polyn_data_for0};
 
 always @(posedge clk or posedge rst)
 	if(rst) 
-		random <= 16'b0;
+		random <= 16'b1;
 	else begin 
 		random <= data_out;
+		if(data_out == 16'hA541) 
+			$display("11");
 	end
-
-always @(*) begin 
-	data_out = {random[15:0],polyn_data||polyn_data_for0};
-end
   
 endmodule
