a3bf3e3f5499267511a9550be66daffaaf6ef8ac
Tatev2919 <tatev@karapetian.ru>
2021-01-18 16:29:07 +0400

    aaa

--- hamming_code.v
+++ hamming_code.v
@@ -1,6 +1,7 @@
 module hamming_code(
 	clk,rst,data_in,data_out
 );
+
 input clk,rst;
 input [7:0] data_in;
 output [11:0] data_out;
@@ -9,27 +10,35 @@ reg [11:0] data_gen;
 reg [4:0] i,k;
 reg [2:0] m;
 
-always @(posedge clk or posedge rst) begin
+always @(rst or data_in) begin
 	if (rst) begin 
-		i <= 5'b0;
-		m <= 3'b0;	
-		k <= 5'b0;
-		data_gen <= 12'b0;
+		data_gen = 12'b0;
 	end 
 	else begin
-		if(i < 11 && m < 3 && i >=0) begin 
+		i = 5'b0;
+		m = 3'b0;	
+		k = 5'b0;
+		while (i < 5'd11 && k < 5'd8) begin 
 			if(i == (2**m)-1) begin
-				data_gen[i] <= 1'b1;
-				m <= m +1;
-				k <= k +1;
+				data_gen[i] = 1'b0;
+				m = m + 3'b1;
 			end
 			else begin
-				data_gen[i] <= data_in[k];
-				k <= k+1;
+				data_gen[i] = data_in[k];
+				k = k + 5'b1;
 			end	
+			i = i + 5'b1;
 		end
-		else 
-			i <= i + 1;
+
+		if(^(data_gen & 12'b101010101010))  
+			data_gen[0] = ~data_gen[0];
+		if(^(data_gen & 12'b001100110011))  
+			data_gen[1] = ~data_gen[1];
+		if(^(data_gen & 12'b000111100001)) 
+			data_gen[3] = ~data_gen[3];
+		if(^(data_gen & 12'b000000001111)) 
+			data_gen[7] = ~data_gen[7];
 	end 
 end
+
 endmodule
