7ac29471e51556f5610c136654b60ad3ad6a26d3
= <=>
2021-01-19 15:43:04 +0400

    v1

--- hamming_decoder.v
+++ hamming_decoder.v
@@ -1,11 +1,17 @@
-module hamming_decoder(output reg [0:20]data_out,
-output reg [4:0]pout,
-input [0:20]data_in);
-reg [4:0]S=1;
+module hamming_decoder(output reg [15:0]data_out1,
+output reg err,
+input [20:0]data_in1);
+reg [4:0]S;
+reg [0:15] data_out;
+reg [0:20] data_in;
+reg [4:0]pout;
 reg [4:0]k;
 always@(*)
-begin
-	data_out = data_in;
+begin 
+       data_in = data_in1;
+
+        S=0;
+	data_out =0 ;
         pout[0]= data_in[2]^data_in[4]^data_in[6]^data_in[8]^data_in[10]
 	           ^data_in[12]^data_in[14]^data_in[16]^data_in[18]
 		   ^data_in[20];
@@ -23,9 +29,15 @@ begin
 	if(pout[k]!==data_in[2**k-1])begin
 		S =S+ (2**k);
 	end
-       data_out={pout[0],pout[1],data_in[2],pout[2],data_in[4:6],pout[3],
-	       data_in[8:14],pout[4],data_in[16:20]};
-       data_out[S-1]=~data_in[S-1];
+       data_out={data_in[2],data_in[4:6],
+	       data_in[8:14],data_in[16:20]};
+       data_out[S-5]=~data_in[S-5];
+       if(S!=0)begin
+	       err = 1;
+       end else begin
+	       err = 0;
+       end
+       data_out1 = data_out;
 end
 
 
--- hamming_decoder_tb.v
+++ hamming_decoder_tb.v
@@ -1,17 +1,17 @@
 module hamming_decoder_tb;
-reg [20:0] data_in;
-wire [4:0]pout;
-wire [20:0] data_out;
+reg [20:0] data_in1;
+wire err;
+wire [15:0] data_out1;
 
 hamming_decoder hamm_dec(
-.data_in(data_in),
-.pout(pout),
-.data_out(data_out)
+.data_in1(data_in1),
+.err(err),
+.data_out1(data_out1)
 );
 initial
 begin
 //#10	data_in = 21'b000010001100001011101;
-#15 data_in = 21'b100110000100001011101; 
+#15 data_in1 = 21'b100110000110001011101; 
 #1000 $finish;
 end
 initial
